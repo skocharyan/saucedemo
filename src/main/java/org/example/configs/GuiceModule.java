a92134f243bf0d7a7ffecd4056ae09809deaa029
skocharyan <66318455+skocharyan@users.noreply.github.com>
2021-01-10 14:43:38 +0300

    add BasePage and LoginPage

--- src/main/java/org/example/configs/GuiceModule.java
+++ src/main/java/org/example/configs/GuiceModule.java
@@ -1,10 +1,12 @@
 package org.example.configs;
 
 import com.google.inject.AbstractModule;
+import org.example.factories.Factory;
+import org.example.factories.WebDriverFactory;
 
 public class GuiceModule extends AbstractModule {
     @Override
     protected void configure() {
-
+       bind(Factory.class).to(WebDriverFactory.class);
     }
 }
